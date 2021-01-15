708da9d818c74a3bd1330d10682e6a604779b462
statick64 <kennethisong@gmail.com>
2021-01-15 16:39:01 +0100

    added status

---
+++ rest/database_helper.py
@@ -0,0 +1,79 @@
+import sqlite3
+import psycopg2
+import os
+
+class MemberDB:
+    def __init__(self,user_name,password,chy_points="0",cycles="0",vip="0",name="user",last_scraped="0",status="0", consumption_point= "0"):
+        self.user_name = user_name
+        self.password = password
+        self.chy_points = chy_points
+        self.status = status
+        self.consumption_point = consumption_point
+        self.cycles = cycles
+        self.vip = vip
+        self.name = name
+        self.last_scraped = last_scraped
+        
+
+
+class DatabaseHelper:
+    def __init__(self):
+        #self.db = sqlite3.connect("../db.sqlite3")
+        #self.c = self.db.cursor()
+        #self.member = MemberDB("user","password")
+
+
+        DATABASE_URL = os.environ['DATABASE_URL']
+        self.db = psycopg2.connect(DATABASE_URL,sslmode='require')
+        self.c = self.db.cursor()
+    
+
+
+
+    def get_members_credentials(self):
+        members_q = self.c.execute("""
+        SELECT user_name, password
+        FROM rest_member
+        """
+        ) 
+        members_t = self.c.fetchall()
+        members = [ MemberDB(x[0],x[1]) for x in members_t]
+        return members
+    def insert_member(self,member):
+        try:
+            self.c.execute("""
+            UPDATE rest_member
+            SET chy_points = %s,
+            cycles =%s,
+            vip = %s,
+            status = %s,
+            consumption_point = %s,
+            last_scrapped = %s
+            WHERE user_name = %s;
+            """,
+            (member.chy_points,member.cycles,member.vip,member.status,member.consumption_point,member.last_scraped,member.user_name))
+        except Exception as e:
+            print(str(e))
+        self.db.commit()
+    
+    def auth_error(self,member):
+        try:
+            self.c.execute("""
+            UPDATE rest_member
+            SET chy_points = 'error !',
+            cycles = 'error !',
+            vip = 'error !'
+            WHERE user_name = %s;
+            """,
+            (member.user_name,))
+        except Exception as e:
+            print(str(e))
+
+
+
+
+
+
+
+        
+    
---
+++ rest/scraper.py
@@ -0,0 +1,117 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from database_helper import MemberDB,DatabaseHelper
+import os
+import datetime
+
+
+
+class ChyBot:
+
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        #self.driver = webdriver.Chrome(executable_path = "chromedriver.exe")
+
+    def go_to_login(self):
+        bot.driver.find_element_by_xpath(
+            "/html/body/div/header/div/div/div[1]/div/a[1]").click()
+
+    def sign_in(self, username,password):
+        self.driver.get("https://www.chymall.net/Mall/Login")
+        self.driver.implicitly_wait(6)
+        user_name = self.driver.find_element_by_xpath(
+            "/html/body/div[1]/form/div/div[2]/ul/li[1]/input")
+        user_password = self.driver.find_element_by_xpath(
+            "/html/body/div[1]/form/div/div[2]/ul/li[2]/input")
+        button = self.driver.find_element_by_xpath(
+            "/html/body/div[1]/form/div/div[2]/button")
+        user_name.click()
+        user_name.send_keys(username)
+        user_password.click()
+        user_password.send_keys(password)
+        button.click()
+
+    def check_chy_point(self):
+        # hasLoaded = self.delay_url()
+        welcome = self.delay("/html/body/div/header/div/div/span")
+        # if hasLoaded:
+        if welcome:
+            self.driver.get("https://www.chymall.net/mall/UserCenter/Index")
+            value = self.delay("/html/body/div/div[2]/div/div[1]/div/div[2]/div/form/ul/li[9]/input")
+            text = value.get_attribute("value")
+            return text
+
+    def Vip(self):
+        value = self.delay("/html/body/div/div[2]/div/div[1]/div/div[2]/div/form/ul/li[5]/input")
+        text = value.get_attribute("value")
+        return text
+
+    def cumsumption_point(self):
+        value = self.delay("/html/body/div/div[2]/div/div[1]/div/div[2]/div/form/ul/li[10]/input")
+        text = value.get_attribute("value")
+        return text    
+
+    def status(self):
+        self.driver.get("https://www.chymall.net/mall/Order/MyOrder")
+        value = self.delay("/html/body/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/div[3]/span")
+        text = value.get_attribute("value")
+        if text in str(range(0,10)):
+            return text
+        else: 
+            return "sold"    
+
+
+    def check_countdown(self):
+        self.driver.get("https://www.chymall.net/mall/Order/MyOrder")
+        value = self.delay("/html/body/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/p[2]")
+        return value.text
+
+    def delay_click(self,path):
+        element = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, path))
+        )
+        return element
+    def delay(self,path):
+        element = WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, path))
+        )
+        return element
+    def delay_url(self):
+        hasLoaded = WebDriverWait(self.driver, 30).until(
+            EC.url_matches("https://chymall.net/")
+        )
+        return hasLoaded
+
+
+
+
+
+bot = ChyBot()
+d = DatabaseHelper()
+members = d.get_members_credentials()
+
+for member in members:
+    try:
+        bot.sign_in(member.user_name,member.password)
+        member.chy_points =   bot.check_chy_point()
+        member.vip = bot.Vip()
+        member.cycles = bot.check_countdown()
+        member.countdown = bot.status()
+        member.queue = bot.cumsumption_point()
+        member.last_scraped = str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M"))
+        d.insert_member(member=member)
+    except Exception as e:
+        d.auth_error(member)
+
+print("Scrapped")
+    
