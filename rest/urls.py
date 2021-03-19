961465f8161cca3322a34408aa1dabe3850667c3
emem785 <32265358+emem785@users.noreply.github.com>
2021-03-19 16:43:28 +0100

    seting settings

--- rest/urls.py
+++ rest/urls.py
@@ -4,6 +4,7 @@ from .views import ModifyMember
 
 urlpatterns = [
     path("get_members",views.get_all_members,name="get-all-members"),
+    path("get_mems",views.get_all_mems,name="get-all-members"),
     path("create_member",views.create_member,name="create-member"),
     path("modify_member/<str:pk>",ModifyMember.as_view(),name="modify-member"),
 ]
